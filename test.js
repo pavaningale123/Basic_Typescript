3fc7a243b92ca5dcff4451ad0e7bdb61c991073e
Pavan Ingale <pavaningale7849@gmail.com>
2026-04-20 01:01:30 +0530

    typescripts pgm

---
+++ test.js
@@ -0,0 +1,6 @@
+"use strict";
+function addnumber(a, b) {
+    return a + b;
+}
+let sum = addnumber(2, 3);
+console.log(sum);
---
+++ test.ts
@@ -0,0 +1,6 @@
+function addnumber(a:number,b:number){
+    return a+b;
+}
+
+let sum:number = addnumber(2,3);
+console.log(sum);
