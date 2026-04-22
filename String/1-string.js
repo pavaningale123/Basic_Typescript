cf6e1d7e7e9331183f6761f52c58a5877022e49e
Pavan Ingale <pavaningale7849@gmail.com>
2026-04-22 11:04:29 +0530

    22-4-26

---
+++ String/1-string.js
@@ -0,0 +1,21 @@
+"use strict";
+var str = "welcome";
+//chatAt()
+console.log("chatAt:", str.charAt(2));
+//concat()
+var s1 = "welcome";
+var s2 = "to javascript";
+console.log("concat", s1.concat(s2));
+//replace()
+var s3 = "Typescript Programming";
+console.log("replace", s3.replace('i', 'X'));
+// split
+var fruits = "Apple Banana Orange";
+console.log('split', fruits.split(" "));
+//substring
+var s4 = "welcome";
+console.log('substring', s4.substring(1, 4));
+// toUpperCase and toLowerCase
+var s5 = "Welcome";
+console.log('toUpperCase', s5.toUpperCase());
+console.log('toLowerCase', s5.toLowerCase());
---
+++ String/1-string.ts
@@ -0,0 +1,35 @@
+var str:string = "welcome";
+
+//chatAt()
+
+console.log("chatAt:", str.charAt(2));
+
+//concat()
+
+var s1:string = "welcome"
+var s2:string="to javascript"
+
+console.log("concat", s1.concat(s2));
+
+//replace()
+
+var s3="Typescript Programming"
+
+console.log("replace",s3.replace('i', 'X'));
+
+// split
+
+var fruits:string = "Apple Banana Orange";
+console.log('split',fruits.split(" "));
+
+//substring
+
+var s4="welcome";
+
+console.log('substring',s4.substring(1,4));
+
+// toUpperCase and toLowerCase
+
+var s5:string="Welcome"
+console.log('toUpperCase', s5.toUpperCase());
+console.log('toLowerCase', s5.toLowerCase());
